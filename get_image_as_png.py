12909d71febbfaff05a02c56d2b2f01ba0e7dc73
MrMegnis <mr.megnis@mail.ru>
2023-01-26 19:01:03 +0500

    add parsing a map

--- get_image_as_png.py
+++ get_image_as_png.py
@@ -1,6 +1,7 @@
 import requests
 
-def get_image_as_png(addres, size, path, api_key = "40d1649f-0493-4b70-98ba-98533de7710b"):
+
+def get_image_as_png(addres, size, path, api_key="40d1649f-0493-4b70-98ba-98533de7710b"):
     url = f"https://geocode-maps.yandex.ru/1.x?geocode={addres}&apikey={api_key}&format=json"
     geocoder_params = {
         "apikey": api_key,
